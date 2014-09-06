a5e53a6c84f09c5dfc732565e99c8fc9e27138b6
gshivani <gshivani@ccs.neu.edu>
2014-09-06 17:10:24 -0400

    notebook update

--- Ex3.rkt
+++ Ex3.rkt
@@ -3,7 +3,11 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname Ex3) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 (define (f->c f)
   ( * (- f 32) (/ 5 9)))
-(f->c 32)
-(f->c 100)
-(f->c 76)
-(f->c 72)
+
+
+(check-expect (f->c 32) 0)
+(check-within (f->c 100) 37.7 5 )
+(check-within (f->c 72) 22.2 5 )
+
+
+
