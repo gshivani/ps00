e7858b07dfb4457524da0368a56b9f9c81da385e
gshivani <gshivani@ccs.neu.edu>
2014-09-06 18:04:51 -0400

    name changes

--- 6.rkt
+++ 6.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname Ex6) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |6|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 ; quadratic-root : Number Number Number -> Number
 ; GIVEN: Co-efficients of a quadratic equation
 ; RETURNS: The root of the quadratic equation
@@ -12,7 +12,9 @@
   (/( + (- b) (sqrt (-(* b b) (* 4 a c)))) (* 2 a)))
 
 
-(quadratic-root 2 4 4)
-(quadratic-root 5 4 4)
+(check-within (quadratic-root 2 4 4) -1+1i 4)
+(check-within (quadratic-root 5 4 4) -0.4+0.8i 2)
+
+
 
    
